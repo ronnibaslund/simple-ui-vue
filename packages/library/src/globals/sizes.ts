8e9564cc547d24139f28124e8e75417d31cb5aa2
Ronni Baslund <rhb@vdoc.dk>
2025-04-17 16:09:17 +0200

    feat: add icon sizes to sizes module for enhanced flexibility

--- packages/library/src/globals/sizes.ts
+++ packages/library/src/globals/sizes.ts
@@ -6,3 +6,9 @@ export type XlSizes = (typeof xlSizes)[number]
 
 export const extendedSizes = [...sizes, ...xlSizes] as const
 export type ExtendedSizes = (typeof extendedSizes)[number]
+
+export const iconSizes = [
+  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72,
+  80, 96
+] as const
+export type IconSizes = (typeof iconSizes)[number]
